5b9327294470953c623bb3b1b98444007c730de1
inesaf <ines.a.fernands@gmail.com>
2014-11-01 16:28:20 +0000

    Inverted index where each word has a list of docIDs (int)

--- InfoRetrieval/src/ProcessingDoc3.java
+++ InfoRetrieval/src/ProcessingDoc3.java
@@ -1,77 +1,91 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class ProcessingDoc3 {
+	static String path = "/home/ines/Repository";
+	static File docsRepository = new File(path);
 
 	public static void main(String[] args) {
-		TreeMap<String, Integer> invertedIndex = new TreeMap<String, Integer>( );  
-		readWordDoc(invertedIndex);  
-		printAllCounts(invertedIndex);  
+		TreeMap<String, List<Integer>> invertedIndex = new TreeMap<String, List<Integer>>( ); //instead of integer put a list of documents
+		File[] filesList = docsRepository.listFiles();
+		Arrays.sort(filesList);
+		readWordDoc(invertedIndex, filesList);  
+		printAllCounts(invertedIndex); 
+		invertedIndex.clear();
 	}
 
 
-	private static void readWordDoc(TreeMap<String, Integer> invertedIndex) {
+
+	private static void readWordDoc(TreeMap<String, List<Integer>> invertedIndex, File[] docsList) {
 		Scanner doc;  
-		String word;     // A word read from the file  
-		Integer count;   // The number of occurrences of the word
-
+		int totalNoDocs = docsList.length;
+		
+		System.out.println("Total number of documents to index " + totalNoDocs);
+		
 		//**FOR LOOP TO READ THE DOCUMENTS**  
-		//for (int x=0; x<Docs.length; x++) 
-		try  
-		{  
-			doc = new Scanner(new FileReader("/home/ines/Repository/pg11.txt"));  
-		}  
-		catch (FileNotFoundException e)  
-		{  
-			System.err.println(e);  
-			return;
-		}
+		for (int i = 0; i < totalNoDocs; i++){
+			String docName= docsList[i].getName().replaceAll("\\D+","");
+			System.out.println(docName);
+			int docID = Integer.parseInt(docName);
+			try {  
+				doc = new Scanner(new FileReader(docsList[i].getPath()));  
+			}  
+			catch (FileNotFoundException e) {  
+				System.err.println(e);  
+				return;
+			}
 
-		while (doc.hasNextLine()) {  
-			String line = doc.nextLine();
-			if(line.contains("*** START OF THIS PROJECT GUTENBERG"))
-				continue;				
-			else if (line.contains("*** END OF THIS PROJECT GUTENBERG"))
-				break;
+			while (doc.hasNextLine()) {  
+				String line = doc.nextLine();
+				if(line.contains("START OF THIS PROJECT GUTENBERG"))
+					continue;				
+				else if (line.contains("END OF THIS PROJECT GUTENBERG"))
+					break;
 
-			else 
-				putIntoInvertedIndex(invertedIndex, line);
+				else 
+					putIntoInvertedIndex(invertedIndex, line, docID);
+			}
 		}
 	}
 
-	private static void putIntoInvertedIndex(TreeMap<String, Integer> invertedIndex, String line) {
-		Integer count;   // The number of occurrences of the word
-		
-		for (String _word : line.split("\\W+")) {
-			String word = _word.toLowerCase();
-			// Get the current count of this word, add one, and then store the new count:  
-			count = getCount(word, invertedIndex) + 1;  
-			invertedIndex.put(word, count); 	
+	private static void putIntoInvertedIndex(TreeMap<String, List<Integer>> invertedIndex, String line, int docID) {
+
+		for (String word : line.split("\\W+")) {
+			String key = word.toLowerCase();
+			List<Integer> postings = invertedIndex.get(key);
+			if (postings == null){
+				postings = new LinkedList<Integer>(); 
+				invertedIndex.put(key, postings);
+				postings.add(new Integer(docID));
+			}
+			else {
+				if(!invertedIndex.get(key).contains(docID)) //the doc could already be listed
+					postings.add(new Integer(docID));
+			}
 		}
-		
+
 	}
 
-
-	private static int getCount(String word, TreeMap<String, Integer> frequencyData){  
-		if (frequencyData.containsKey(word)){  // The word has occurred before, so get its count from the map  
-			return frequencyData.get(word);  
-		}  
-		else {  // No occurrences of this word  
-			return 0;  
-		}  
-	}
-
-	private static void printAllCounts(TreeMap<String, Integer> invertedIndex) {
+	private static void printAllCounts(TreeMap<String, List<Integer>> invertedIndex) {
 		System.out.println("-----------------------------------------------");  
 		System.out.println("    Occurrences    Word");  
 
-		for(String word : invertedIndex.keySet( ))  
-		{  
-			System.out.printf("%15d    %s\n", invertedIndex.get(word), word);  
+		for(String key : invertedIndex.keySet()){
+			System.out.printf("\n%s", key);
+			//for(Integer docID : invertedIndex.get(key))
+			System.out.printf(" -> %5s", invertedIndex.get(key).toString());  
 		}  
-
+		
+		System.out.printf("\nTotal number distinct words: %15d\n", invertedIndex.size());  
+		
 		System.out.println("-----------------------------------------------");  
 	}
+	
+	
 }
