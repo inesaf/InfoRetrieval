bd9a54501fc1b85531d35740b0479c44cc828643
inesaf <ines.a.fernands@gmail.com>
2014-11-04 15:50:26 +0000

    Ask to the user if he wishes to process the documents, asking for a path to the respository.
    Prompts the user to enter a query.
    
    Comments of each method

--- InfoRetrieval/src/ProcessingDoc3.java
+++ InfoRetrieval/src/ProcessingDoc3.java
@@ -18,41 +18,73 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public class ProcessingDoc3 {
-	static String path = "/home/ines/Repository";
-	static File docsRepository = new File(path);
-	static Map<String, Integer> dictionary = new HashMap<String, Integer>();
+	
 
 	public static void main(String[] args) {
-		TreeMap<String, List<Integer>> invertedIndex = new TreeMap<String, List<Integer>>( ); //instead of integer put a list of documents
+		Scanner in = new Scanner(System.in); //perguntar se é necessário realizar o processamento dos dados
+		
+		System.out.println("Do you wish to process documents to create a new inverted index? Y/N");
+		String a = in.nextLine();
+		if(a.equalsIgnoreCase("y")){
+			System.out.println("The default path is '/home/ines/Repository'. Do you wish to change it? Y/N");
+			String b = in.nextLine();
+			if(b.equalsIgnoreCase("y")){
+				System.out.println("Specify the new path: ");
+				String path = in.nextLine();
+				processingDocs(path);
+			}
+			if(b.equalsIgnoreCase("n"))
+				processingDocs("/home/ines/Repository");
+		}
+		
+		System.out.println("\nEnter a query: ");
+		String query = in.nextLine();
+		System.out.println(query);
+		
+	}
+
+	/**
+	 * Processing documents gathered into Repository defined by the path
+	 */
+	private static void processingDocs(String path) {
+		
+		TreeMap<String, List<Integer>> invertedIndex = new TreeMap<String, List<Integer>>( ); //inverted index with key and postings
+		Map<String, Integer> dictionary = new HashMap<String, Integer>(); //dictionary where the string corresponds to a word in the text and the Integer to its collection frequency
+		File docsRepository = new File(path); //files found into the repository
 		File[] filesList = docsRepository.listFiles();
 		Arrays.sort(filesList);
-		readWordDoc(invertedIndex, filesList);  
+		readWordDoc(invertedIndex, dictionary, filesList);  
 		//printAllCounts(invertedIndex); 
-		Map<String, Integer> sortedDictionary = sortByComparator(dictionary);
 		//printMap(sortedDictionary);
 		try {
-			printIndex(invertedIndex);
-			printDicToFile(sortedDictionary);
+			printInvertedIndexToFile(invertedIndex);
+			printDicToFile(dictionary);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		dictionary.clear();
 		invertedIndex.clear();
+		
 	}
 
 
-
-	private static void readWordDoc(TreeMap<String, List<Integer>> invertedIndex, File[] docsList) {
+	
+	
+	
+	/**
+	 * Document tokenization where each token/word is added to the inverted index and dictionary 
+	 */
+	private static void readWordDoc(TreeMap<String, List<Integer>> invertedIndex, Map<String, Integer> dictionary, File[] docsList) {
 		Scanner doc;  
 		int totalNoDocs = docsList.length;
 		
 		System.out.println("Total number of documents to index " + totalNoDocs);
 		
-		//**FOR LOOP TO READ THE DOCUMENTS**  
+		//loop to read all documents (identified by their Project Gutenberg id) 
 		for (int i = 0; i < totalNoDocs; i++){
 			String docName= docsList[i].getName().replaceAll("\\D+","");
-			System.out.println(docName);
+			System.out.println("Processing file: " + docName);
 			int docID = Integer.parseInt(docName);
 			try {  
 				doc = new Scanner(new FileReader(docsList[i].getPath()));  
@@ -61,21 +93,25 @@ public class ProcessingDoc3 {
 				System.err.println(e);  
 				return;
 			}
-
+			
+			// the header and footer is discarded
 			while (doc.hasNextLine()) {  
 				String line = doc.nextLine();
 				if(line.contains("START OF THIS PROJECT GUTENBERG"))
 					continue;				
 				else if (line.contains("END OF THIS PROJECT GUTENBERG"))
 					break;
-
 				else 
-					putIntoInvertedIndex(invertedIndex, line, docID);
+					putIntoStructures(invertedIndex, dictionary, line, docID);
 			}
 		}
 	}
 
-	private static void putIntoInvertedIndex(TreeMap<String, List<Integer>> invertedIndex, String line, int docID) {
+	/**
+	 * Line by line each word is added to the inverted index and the document is added to its list
+	 * In parallel each word is also added to the dictionary where its frequency collection is defined
+	 */
+	private static void putIntoStructures(TreeMap<String, List<Integer>> invertedIndex, Map<String, Integer> dictionary, String line, int docID) {
 
 		for (String word : line.split("\\W+")) {
 			String key = word.toLowerCase();
@@ -96,23 +132,12 @@ public class ProcessingDoc3 {
 		}
 
 	}
-
-	private static void printAllCounts(TreeMap<String, List<Integer>> invertedIndex) {
-		System.out.println("-----------------------------------------------");  
-		System.out.println("    Occurrences    Word");  
-
-		for(String key : invertedIndex.keySet()){
-			System.out.printf("\n%s", key);
-			//for(Integer docID : invertedIndex.get(key))
-			System.out.printf(" -> %5s", invertedIndex.get(key).toString());  
-		}  
-		
-		System.out.printf("\nTotal number distinct words: %15d\n", invertedIndex.size());  
-		
-		System.out.println("-----------------------------------------------");  
-	}
 	
-	private static void printIndex(TreeMap<String, List<Integer>> invertedIndex) throws IOException {
+	
+	/**
+	 * Print the inverted index into a file  
+	 */
+	private static void printInvertedIndexToFile(TreeMap<String, List<Integer>> invertedIndex) throws IOException {
 
 		File fout = new File("inverted_index.txt");
 		FileOutputStream fos = new FileOutputStream(fout);
@@ -130,6 +155,10 @@ public class ProcessingDoc3 {
 		bw.close();
 	}
 	
+	
+	/**
+	 * Sort words in the dictionary by descending order of frequency
+	 */
 	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
 		 
 		// Convert Map to List
@@ -152,19 +181,16 @@ public class ProcessingDoc3 {
 		}
 		return sortedMap;
 	}
- 
-	public static void printMap(Map<String, Integer> map) {
-		for (Map.Entry<String, Integer> entry : map.entrySet()) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
-		}
-	}
 	
+	/**
+	 * Print dictionary of words and collection frequency into a file  
+	 */
 	public static void printDicToFile(Map<String, Integer> dic) throws FileNotFoundException {
 		File fout = new File("dictionary.txt");
 		FileOutputStream fos = new FileOutputStream(fout);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-		
-		for (Map.Entry<String, Integer> entry : dic.entrySet()) {
+		Map<String, Integer> sortedDic = sortByComparator(dic);
+		for (Map.Entry<String, Integer> entry : sortedDic.entrySet()) {
 			try {
 				bw.write(entry.getKey() + " : " + entry.getValue());
 				bw.newLine();
@@ -174,5 +200,31 @@ public class ProcessingDoc3 {
 		}
 	}
 	
+	/**
+	 * Auxiliary method to print the inverted index in the console  
+	 */
+	private static void printInvertedIndex(TreeMap<String, List<Integer>> invertedIndex) {
+		System.out.println("-----------------------------------------------");  
+		System.out.println("    Occurrences    Word");  
+
+		for(String key : invertedIndex.keySet()){
+			System.out.printf("\n%s", key);
+			//for(Integer docID : invertedIndex.get(key))
+			System.out.printf(" -> %5s", invertedIndex.get(key).toString());  
+		}  
+		
+		System.out.printf("\nTotal number distinct words: %15d\n", invertedIndex.size());  
+		
+		System.out.println("-----------------------------------------------");  
+	}
+	
+	/**
+	 * Auxiliary method to print the inverted index in the console  
+	 */
+	private static void printMap(Map<String, Integer> map) {
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
 	
 }
