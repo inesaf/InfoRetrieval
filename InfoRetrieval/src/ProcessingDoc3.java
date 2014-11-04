eb75fa5f0b0db7561133c1da1f36d14c1f3bd2d3
inesaf <ines.a.fernands@gmail.com>
2014-11-04 14:05:24 +0000

    Print to file "inverted_index.txt" the inverted index in the format of key [doc, doc, doc, ...].
    Print to file "dictionary.txt" key : collection_frequency

--- InfoRetrieval/src/ProcessingDoc3.java
+++ InfoRetrieval/src/ProcessingDoc3.java
@@ -1,22 +1,43 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class ProcessingDoc3 {
 	static String path = "/home/ines/Repository";
 	static File docsRepository = new File(path);
+	static Map<String, Integer> dictionary = new HashMap<String, Integer>();
 
 	public static void main(String[] args) {
 		TreeMap<String, List<Integer>> invertedIndex = new TreeMap<String, List<Integer>>( ); //instead of integer put a list of documents
 		File[] filesList = docsRepository.listFiles();
 		Arrays.sort(filesList);
 		readWordDoc(invertedIndex, filesList);  
-		printAllCounts(invertedIndex); 
+		//printAllCounts(invertedIndex); 
+		Map<String, Integer> sortedDictionary = sortByComparator(dictionary);
+		//printMap(sortedDictionary);
+		try {
+			printIndex(invertedIndex);
+			printDicToFile(sortedDictionary);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		dictionary.clear();
 		invertedIndex.clear();
 	}
 
@@ -63,10 +84,14 @@ public class ProcessingDoc3 {
 				postings = new LinkedList<Integer>(); 
 				invertedIndex.put(key, postings);
 				postings.add(new Integer(docID));
+				dictionary.put(key, 1);
 			}
 			else {
-				if(!invertedIndex.get(key).contains(docID)) //the doc could already be listed
+				if(!invertedIndex.get(key).contains(docID)){ //the doc could already be listed
 					postings.add(new Integer(docID));
+					int count = dictionary.get(key);
+					dictionary.put(key, ++count);
+				}
 			}
 		}
 
@@ -85,6 +110,68 @@ public class ProcessingDoc3 {
 		System.out.printf("\nTotal number distinct words: %15d\n", invertedIndex.size());  
 		
 		System.out.println("-----------------------------------------------");  
+	}
+	
+	private static void printIndex(TreeMap<String, List<Integer>> invertedIndex) throws IOException {
+
+		File fout = new File("inverted_index.txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+	 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+	 
+		for(String key : invertedIndex.keySet()){
+			bw.write(key + " ");
+			bw.write(invertedIndex.get(key).toString());
+			bw.newLine();
+		}  
+		
+		System.out.printf("\nTotal number distinct words: %15d\n", invertedIndex.size());  
+	 
+		bw.close();
+	}
+	
+	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap) {
+		 
+		// Convert Map to List
+		List<Map.Entry<String, Integer>> list = 
+			new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+ 
+		// Sort list with comparator, to compare the Map values
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1,
+                                           Map.Entry<String, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+ 
+		// Convert sorted map back to a Map
+		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext();) {
+			Map.Entry<String, Integer> entry = it.next();
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		return sortedMap;
+	}
+ 
+	public static void printMap(Map<String, Integer> map) {
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+	
+	public static void printDicToFile(Map<String, Integer> dic) throws FileNotFoundException {
+		File fout = new File("dictionary.txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		
+		for (Map.Entry<String, Integer> entry : dic.entrySet()) {
+			try {
+				bw.write(entry.getKey() + " : " + entry.getValue());
+				bw.newLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	
